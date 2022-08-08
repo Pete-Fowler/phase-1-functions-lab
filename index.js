bad743d1dc4dfcc6564f253a74ad8e75e210df1a
Pete Fowler <fowler.pete@gmail.com>
2022-08-08 11:24:54 -0600

    commit tree 3441cded8144bdbf5defb90cceee7faa84b1fbbf

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+  let blocks = street - 42;
+  if(blocks <= 0) {
+    blocks *= -1;
+  }
+  return blocks;
+}
+
+function distanceFromHqInFeet(street) {
+  return distanceFromHqInBlocks(street) * 264;
+}
+
+function distanceTravelledInFeet(a, b) {
+  let dist = (a - b) * 264;
+  if(dist <= 0) {
+    dist *= -1;
+  }
+  return dist;
+}
+
+function calculatesFarePrice(a, b) {
+  const feet = distanceTravelledInFeet(a, b);
+  if(feet < 400) {
+      return 0;
+  } else if(feet <= 2000) {
+      return Math.round((.02 * feet - 8) * 100) / 100;
+  } else if(feet < 2500) {
+      return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
